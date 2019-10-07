4495a1e6504f7732d2ef015a7934438337e63c8e
keyvankhademi <keyvankhademi@gmail.com>
2019-10-06 21:08:21 -0700

    add word cloud

---
+++ HCI_Course/HCI/management/commands/word_cloud.py
@@ -0,0 +1,20 @@
+from django.core.management.base import BaseCommand
+import string
+import numpy as np
+from os import path
+from PIL import Image
+from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
+import matplotlib.pyplot as plt
+
+from HCI.models import University, Course, Criteria, Topic
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        text = " ".join(course.description for course in Course.objects.all())
+        text = text.translate({ord(key): None for key in string.punctuation})
+
+        wordcloud = WordCloud().generate(text)
+        plt.imshow(wordcloud, interpolation='bilinear')
+        plt.axis("off")
+        plt.show()
