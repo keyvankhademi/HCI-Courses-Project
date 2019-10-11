d52d1bb247cacfbf536c080bdedf856b207218dd
keyvankhademi <keyvankhademi@gmail.com>
2019-10-10 18:46:49 -0700

    User authentication

--- HCI_Course/HCI/views.py
+++ HCI_Course/HCI/views.py
@@ -1,7 +1,25 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+
 
 # Create your views here.
 
 
 def homepage(request):
     return render(request, "base.html")
+
+
+def signup_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('homepage')
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
--- HCI_Course/HCI_Course/urls.py
+++ HCI_Course/HCI_Course/urls.py
@@ -16,8 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from HCI import views
+from django.contrib.auth import views as auth_views
+
+from HCI.views import signup_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.homepage),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('signup/', signup_view, name='signup'),
+    path('', views.homepage, name='homepage'),
 ]
