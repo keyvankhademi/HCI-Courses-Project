f045c2e3b5ce2fb626d400eeece8e7adbfc67f8b
ricksonre <ricksonre@gmail.com>
2019-10-15 20:13:57 -0700

    tuesday

--- HCI_Course/HCI/auxfunctions.py
+++ HCI_Course/HCI/auxfunctions.py
@@ -17,6 +17,7 @@ def get_years():
     years = [course.last_taught.year for course in Course.objects.all()]
     df = pd.Series(years).value_counts()
     data = {
+        'title': "syllabus years histogram",
         'labels': df.keys().tolist(),
         'values': df.tolist()
     }
@@ -34,11 +35,12 @@ def get_terms_freq():
     count = Counter([lem.lemmatize(word).lower() for word in nltk.word_tokenize(desc)
                      if word not in stop])
     data = {
+        'title': "terms histogram",
         'labels': [],
         'values': []
     }
 
-    for x,y in count.most_common(25):
+    for x,y in count.most_common():
         data['labels'].append(x)
         data['values'].append(y)
 
--- HCI_Course/HCI/static/scripts/charts.js
+++ HCI_Course/HCI/static/scripts/charts.js
@@ -1,8 +1,12 @@
 $(document).ready(function()
 {
 
-    d_labels = []
-    d_values = []
+    var d_labels = []
+    var d_values = []
+    var d_title = "";
+    var d_size = 60;
+    var d_type ="";
+    var m_chart = null;
 
     $('#b_year_hist').click(function ()
     {
@@ -12,17 +16,19 @@ $(document).ready(function()
 
                 success: function (data)
                 {
+                    d_title = data.title;
                     d_labels = data.labels;
                     d_values = data.values;
+                    d_size = d_labels.length;
+                    d_type = 'bar'
+                    create_chart()
 
-                    console.log(d_values);
-
-                    create_bar_chart(l = 'amount', t='syllabus years histogram' )
+                    $('#size_input').val(d_size);
                 }
 
             });
 
-
+        $(b_secondary).html('<button id="b_piechart" class="btn btn-secondary">pie chart</button>  <button id="b_barchart" class="btn btn-secondary">bar chart</button>');
     });
 
     $('#b_terms_hist').click(function ()
@@ -35,28 +41,31 @@ $(document).ready(function()
                 {
                     d_labels = data.labels;
                     d_values = data.values;
-
+                    d_size = 50;
+                    d_type = 'bar'
                     console.log(d_values);
 
-                    create_bar_chart(l = 'amount', t = 'terms histogram')
+                    create_chart()
+                    $('#size_input').val(d_size);
                 }
 
             });
-
+        $(b_secondary).html('<button id="b_piechart" class="btn btn-secondary">pie chart</button>   <button id="b_barchart" class="btn btn-secondary">bar chart</button>');
 
     });
 
-    function create_bar_chart(label,title)
+    function create_chart()
     {
-        new Chart(document.getElementById("bar-chart"), {
-            type: 'bar',
+        if(m_chart != null)m_chart.destroy();
+        m_chart = new Chart(document.getElementById("bar-chart"), {
+            type: d_type,
             data: {
-                labels: d_labels,
+                labels: d_labels.slice(0,d_size),
                 datasets: [
                     {
-                        label: l,
+                        label: 'amount',
                         backgroundColor: ["#3e95cd", "#8e5ea2", "#3cba9f", "#e8c3b9", "#c45850"],
-                        data: d_values
+                        data: d_values.slice(0, d_size)
                     }
                 ]
             },
@@ -64,13 +73,30 @@ $(document).ready(function()
                 legend: { display: false },
                 title: {
                     display: true,
-                    text: t
+                    text: d_title
                 }
             }
         });
     }
     
+    $(document).on('click','#b_piechart',function()
+    {
+        d_type = 'pie';
+        create_chart();
+        console.log(1);
+    });
 
+    $(document).on('click', '#b_barchart', function ()
+    {
+        d_type = 'bar';
+        create_chart('bar');
+    });
     
+    $('#size_input').on('input',function()
+    {
+        d_size = this.value;
+        create_chart();
+    })
+
 });
 
