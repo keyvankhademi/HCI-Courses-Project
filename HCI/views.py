83303fc9e6b877463f1a844ea375fff3b10e903a
keyvankhademi <keyvankhademi@gmail.com>
2019-11-05 20:51:28 -0800

    add email confirmation for sign up

--- HCI/views.py
+++ HCI/views.py
@@ -1,19 +1,21 @@
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import login
 from django.contrib.auth.decorators import permission_required
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.mixins import UserPassesTestMixin
 from django.contrib.auth.models import User
 from django.http import HttpResponse, JsonResponse
-from django.shortcuts import render, redirect
-
+from django.shortcuts import render
 # Create your views here.
 from django.urls import reverse_lazy, reverse
 from django.utils.decorators import method_decorator
+from django.utils.encoding import force_text
+from django.utils.http import urlsafe_base64_decode
 from django.views.generic import CreateView, UpdateView, ListView, DetailView
 
+from HCI.forms import SignupForm
 from HCI.models import University, Course
-from HCI.utils.word_cloud import generate_word_cloud
 from HCI.utils import charts
+from HCI.utils.email_functions import send_email, account_activation_token
+from HCI.utils.word_cloud import generate_word_cloud
 
 
 def homepage(request):
@@ -22,16 +24,19 @@ def homepage(request):
 
 def signup_view(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = SignupForm(request.POST)
         if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            raw_password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=raw_password)
-            login(request, user)
-            return redirect('homepage')
+            user = form.save(commit=False)
+            user.is_active = False
+            user.save()
+            send_email(request, user)
+            return render(request, 'message.html', {
+                'title': "Sign Up Successful",
+                'message': "Thank you for signing up. An activation link is sent to your email. Please Activate your "
+                           "account using that link",
+            })
     else:
-        form = UserCreationForm()
+        form = SignupForm()
     return render(request, 'signup.html', {'form': form})
 
 
@@ -167,3 +172,24 @@ class UniversityUpdateView(UpdateView):
 
     def get_success_url(self):
         return reverse('university_detail_view', None, [self.kwargs.get('pk'), ])
+
+
+def activate(request, uidb64, token):
+    try:
+        uid = force_text(urlsafe_base64_decode(uidb64))
+        user = User.objects.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+    if user is not None and account_activation_token.check_token(user, token):
+        user.is_active = True
+        user.save()
+        login(request, user)
+        return render(request, 'message.html', {
+            'title': "Your Account is Activated",
+            'message': 'Thank you for your email confirmation. Now you can login your account.',
+        })
+    else:
+        return render(request, 'message.html', {
+            'title': "Something went wrong",
+            'message': 'Activation link is invalid!',
+        })
