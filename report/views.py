88b0fab62b647ccb0faccba3eaac11acfa81b7ff
keyvankhademi <keyvankhademi@gmail.com>
2019-12-03 19:47:40 -0800

    configure report buttons

--- report/views.py
+++ report/views.py
@@ -1,9 +1,10 @@
 from django.contrib.auth.mixins import UserPassesTestMixin
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 # Create your views here.
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 
+from HCI.models import Course, University
 from message.utils import send_message
 from report.models import CourseReport, UniversityReport
 
@@ -24,6 +25,18 @@ class CourseReportCreateView(UserPassesTestMixin, CreateView):
     fields = ('course', 'reason', 'message')
     success_url = reverse_lazy('report:success')
 
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        course_id = self.request.GET.get('course', None)
+        if not course_id:
+            return kwargs
+        course = get_object_or_404(Course, pk=course_id)
+        kwargs['initial'].update({
+            'course': course,
+        })
+
+        return kwargs
+
     def form_valid(self, form):
         result = super().form_valid(form)
         course_report = self.object
@@ -42,3 +55,14 @@ class UniversityReportCreateView(UserPassesTestMixin, CreateView):
     template_name = 'report/university_report.html'
     fields = ('university', 'reason', 'message')
     success_url = reverse_lazy('report:success')
+
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        university_id = self.request.GET.get('university', None)
+        if not university_id:
+            return kwargs
+        university = get_object_or_404(University, pk=university_id)
+        kwargs['initial'].update({
+            'university': university,
+        })
+        return kwargs
