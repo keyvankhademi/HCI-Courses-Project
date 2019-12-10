69e6bfad4a3ea9751b59aaab7f1dec71987ae876
keyvankhademi <keyvankhademi@gmail.com>
2019-12-09 20:48:59 -0800

    remove sidebar unwilligly

--- HCI/static/main_page/js/main.js
+++ HCI/static/main_page/js/main.js
@@ -20,13 +20,4 @@ jQuery(function ($) {
                 .addClass("active");
         }
     });
-
-    $("#close-sidebar").click(function () {
-        $(".page-wrapper").removeClass("toggled");
-        $(".navbar")[0].style.marginLeft = "0px";
-    });
-    $("#show-sidebar").click(function () {
-        $(".page-wrapper").addClass("toggled");
-        $(".navbar")[0].style.marginLeft = "260px";
-    });
 });
