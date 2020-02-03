b0bf736f35135732fcbe7deb7ce8d1b355b5514e
keyvankhademi <keyvankhademi@gmail.com>
2020-02-02 21:45:49 -0800

    fix the chart bug

--- charts/views.py
+++ charts/views.py
@@ -8,11 +8,28 @@ from HCI.models import Course, Criteria
 
 def criteria_chart_view(request):
 
-    project = Criteria.objects.filter(Q(name__contains='Project') | Q(name__contains='project'))
-    final_exam = Criteria.objects.filter(Q(name__contains='final') | Q(name__contains='Final'))
-    both = project.intersection(final_exam)
+    courses = Course.objects.all()
+
+    project_count = 0
+    final_exam_count = 0
+    both_count = 0
+
+    for course in courses:
+        project = False
+        final_exam = False
+        for criteria in course.criteria_set.all():
+            if 'project' in criteria.name.lower():
+                project = True
+            if 'final' in criteria.name.lower():
+                final_exam = True
+        if project:
+            project_count += 1
+        if final_exam:
+            final_exam_count += 1
+        if project and final_exam:
+            both_count += 1
 
     return render(request, 'charts/criteria_charts.html', {
         'labels': ["Project", "Final Exam", "Both"],
-        'data': [project.count(), final_exam.count(), both.count()],
+        'data': [project_count, final_exam_count, both_count],
     })
